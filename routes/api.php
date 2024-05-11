0fc6a5f8e01607ab9af77fa91fff11d4e42f4a26
pop-culture-studio <pcs.engineer.team@gmail.com>
2024-05-11 14:33:22 +0900

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,26 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('login', function (Request $request) {
+    if (Auth::attempt($request->only('email', 'password'))) {
+        $token = Auth::user()->createToken('user')->plainTextToken;
+        return response()->json(['message' => 'Login', 'token' => $token]);
+    } else {
+        return response()->json(['error' => 'Unauthorized'], 401);
+    }
+})->middleware('guest');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::delete('logout', function (Request $request) {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json(['message' => 'Logout']);
+    });
+});
