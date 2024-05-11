22fba6d8a958bae6384b593d6a9cd8f8329b18f7
pop-culture-studio <pcs.engineer.team@gmail.com>
2024-05-11 13:46:02 +0900

    Update console.php

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,22 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Laravel\Sanctum\PersonalAccessToken;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Artisan::command('token:create {user=1}', function (string $user) {
+    $this->info(User::findOrFail($user)->createToken(name: 'token', abilities: ['admin'])->plainTextToken);
+    $this->warn('APIトークンは一度しか表示されないので必ず保存してください。');
+});
+
+Artisan::command('token:user {token}', function (string $token) {
+    $accessToken = PersonalAccessToken::findToken($token);
+    $user = $accessToken->tokenable->withAccessToken($accessToken);
+    $this->info($user->name);
+    $this->info($user->tokenCan('admin'));
+})->purpose('トークンからユーザーを取得。サンプルでの確認用なので通常はこんな使い方はしない。');
